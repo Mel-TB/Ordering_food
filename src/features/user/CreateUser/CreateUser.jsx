3a8bbfd2824a13eba2a50fb1133f805e82d792d2
Mel-TB <melinda.trami@live.fr>
2023-07-26 17:47:06 +0200

    Implement cart slice and reducer

--- src/features/user/CreateUser/CreateUser.jsx
+++ src/features/user/CreateUser/CreateUser.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import { useNavigate } from 'react-router';
 import { useDispatch } from 'react-redux';
 
-import { updateName } from '../../../store/userSlice';
+import { updateName } from '../userSlice';
 
 import Button from '../../../pages/button/Button';
 
