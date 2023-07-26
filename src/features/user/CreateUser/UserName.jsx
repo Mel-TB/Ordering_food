fc7762553869ffe74894a9d8bc7e45e6584a553d
Mel-TB <melinda.trami@live.fr>
2023-07-26 16:35:53 +0200

    Implement Redux

--- src/features/user/CreateUser/UserName.jsx
+++ src/features/user/CreateUser/UserName.jsx
@@ -1,5 +1,13 @@
+import { useSelector } from 'react-redux';
+
 const UserName = () => {
-  return <div className="hidden text-sm font-semibold md:block"> Mel</div>;
+  const username = useSelector((state) => state.user.username);
+
+  if (!username) return null;
+
+  return (
+    <div className="hidden text-sm font-semibold md:block"> {username}</div>
+  );
 };
 
 export default UserName;
